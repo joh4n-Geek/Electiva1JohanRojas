c5f56b885b0b0c4bbcc8b9f456e8caa39d833ab3
jrojass <jrojass@correo.tdea.edu.co>
2024-03-07 16:56:53 -0500

    add card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+export function Card({title = 'Funny Cat GIF', url = 'https://media2.giphy.com/media/gm3Z05VsyZsFG/200w.webp?cid=53f81e63xh4t9qav4qct4nkkf2s8rwoa4jdba7meth7qid8g&ep=v1_gifs_search&rid=200w.webp&ct=g'}) {
+    return (
+        <div class="card text-bg-dark" style={{width: 200}}>
+            <img src={url} class="card-img" alt={title} />
+            <div class="card-img-overlay">
+                <h5 class="card-title">{ title }</h5>
+            </div>
+        </div>
+    );
+}
