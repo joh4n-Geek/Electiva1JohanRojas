5ef456d4621537c90a202206ac4cb530d2500dc3
jrojass <jrojass@correo.tdea.edu.co>
2024-03-08 23:08:32 -0500

    chg remove comments and unused imports

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import Header from "./components/Header";
 import { ListGifs } from "./components/ListGifs";
 import { SearchBar } from "./components/SearchBar";
-import { getGifsByQuery, getTrendingGifs } from "./components/helpers/getGifs";
 
 export default function MyApp() {
-    // const [inputQuery, setInputQuery] = useState('');
     const [resultGifs, setResultGifs] = useState({
         trending: [],
         search: []
     });
 
-    // useEffect(() => {
-            // setResultGifs({
-            //     trending: []
-            // });
-    // })
-
     return (
-        <div className="container" style={{backgroundColor: 'gray'}}>
-            <h1>Bienvenido a Jiphy App</h1>
+        <div className="container">
+            <Header />
             <SearchBar setResultGifs={setResultGifs} />
             <ListGifs resultGifs={resultGifs} setResultGifs={setResultGifs} />
         </div>
