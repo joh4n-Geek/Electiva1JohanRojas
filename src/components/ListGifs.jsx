84b24f3b25fca28ad0c9e61458e0e357a8f6bce1
jrojass <jrojass@correo.tdea.edu.co>
2024-03-08 23:01:14 -0500

    add create header component, implement grid and glass effect

--- src/components/ListGifs.jsx
+++ src/components/ListGifs.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Card } from "./Card";
 import { getTrendingGifs } from "./helpers/getGifs";
+import Masonry, {ResponsiveMasonry} from "react-responsive-masonry";
+import './styles/ListGifs.css';
 
 export const ListGifs = ({ resultGifs, setResultGifs }) => {
     if (resultGifs.trending.length === 0 && Array.isArray(resultGifs.trending)) {
@@ -13,10 +15,21 @@ export const ListGifs = ({ resultGifs, setResultGifs }) => {
     }
 
     return (
-        <div className="container">
-            {resultGifs.trending.map((gif, id) => {
-                return <Card resultGif={gif} key={id} />;
-            })}
+        <div className="container masonry-grid-card">
+            <ResponsiveMasonry
+                columnsCountBreakPoints={{
+                    1200: 4,
+                    900: 3,
+                    750: 2,
+                    350: 1
+                }}
+            >
+                <Masonry gutter={10}>
+                    {resultGifs.trending.map((gif, id) => {
+                        return <Card resultGif={gif} key={id} />;
+                    })}
+                </Masonry>
+            </ResponsiveMasonry>
         </div>
     );
 }
